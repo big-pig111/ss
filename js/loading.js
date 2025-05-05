3fd5b73c6956d3b7e0e43fb5005c74544f290a57
big-pig111 <xuhang123qwe@gmail.com>
2025-05-05 23:18:26 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -6,7 +6,7 @@ let themes;
 
 function changeLoadingTip() {
     const tips = [
-        'Welcome to the land of Nothing!\n\nWe are launching our governance token: $GG2. ca：000000000000000000000000000000000000000000000',
+        'Welcome to the land of Nothing!\n\nWe are launching our governance token: $GG2.                         ca：000000000000000000000000000000000000000000000',
     ];
     const element = document.getElementsByClassName('loading-tip')[0];
 
